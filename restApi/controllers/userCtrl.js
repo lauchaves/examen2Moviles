36455366818812e96b5a68af04a7c72f82d9e6d1
laurens <laurens004@gmail.com>
2016-06-05 14:25:23 -0600

    api node js mongodb

---
+++ restApi/controllers/userCtrl.js
@@ -0,0 +1,25 @@
+/*
+
+User Controller
+
+*/
+var usersService = require('../businessLogic/usersService.js'); 
+
+exports.getUsers = function(eRequest, eResponse) {
+    usersService.getUsers(function(data){
+        eResponse.send(data);
+    });
+};
+
+exports.getUserById = function(eRequest, eResponse) {
+    usersService.getUserById(eRequest.params.user, function(data){
+        eResponse.send(data);
+    });
+};
+
+exports.newUser = function(eRequest, eResponse) {
+    usersService.newUser(eRequest.body, function(data){
+        eResponse.send(data);
+    });
+};
+
